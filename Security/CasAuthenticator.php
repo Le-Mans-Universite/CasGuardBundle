b6d929a66e970c42a33b331fa860f2031d1722f0
Corentin <corentin.sirou@univ-lemans.fr>
2026-07-15 11:42:36 +0200

    feat. possibility of a CasUserProvider

--- Security/CasAuthenticator.php
+++ Security/CasAuthenticator.php
@@ -3,9 +3,10 @@
 namespace L3\Bundle\CasGuardBundle\Security;
 
 use L3\Bundle\CasGuardBundle\Event\CasAuthenticationFailureEvent;
+use L3\Bundle\CasGuardBundle\Security\CasAttributeStorage;
+use L3\Bundle\CasGuardBundle\Entity\CasUserInterface;
 
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
-use L3\Bundle\CasGuardBundle\Entity\CasUserInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -19,24 +20,26 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Form\Exception\InvalidConfigurationException;
 
-class CasAuthenticator extends AbstractAuthenticator {
+class CasAuthenticator extends AbstractAuthenticator
+{
     protected $config;
-    
+
     private $eventDispatcher;
-    
+
     /**
      * Process configuration
      * @param array $config
      */
-    public function __construct(array $config = Array(), EventDispatcherInterface $eventDispatcher = null) {
+    public function __construct(array $config = array(), EventDispatcherInterface $eventDispatcher = null, private CasAttributeStorage $casAttributeStorage)
+    {
         $this->config = $config;
         $this->eventDispatcher = $eventDispatcher;
     }
 
     public function supports(Request $request): ?bool
     {
-	$session = $request->getSession();
-        
+        $session = $request->getSession();
+
         if ($session->has('impersonate_token')) {
             return false;
         }
@@ -52,24 +55,24 @@ class CasAuthenticator extends AbstractAuthenticator {
      *
      * @return bool
      */
-    public function authenticate(Request $request): Passport 
+    public function authenticate(Request $request): Passport
     {
         $user = "__NO_USER__";
-        
-        if(!isset($_SESSION)) session_start();
 
-	\phpCAS::setLogger();
-	\phpCAS::setVerbose(false);
+        if (!isset($_SESSION)) session_start();
+
+        \phpCAS::setLogger();
+        \phpCAS::setVerbose(false);
 
         \phpCAS::client(CAS_VERSION_2_0, $this->getParameter('host'), $this->getParameter('port'), is_null($this->getParameter('path')) ? '' : $this->getParameter('path'), $this->getParameter('casServiceBaseUrl'), true);
 
-        if(is_bool($this->getParameter('ca')) && $this->getParameter('ca') == false) {
+        if (is_bool($this->getParameter('ca')) && $this->getParameter('ca') == false) {
             \phpCAS::setNoCasServerValidation();
         } else {
             \phpCAS::setCasServerCACert($this->getParameter('ca'));
         }
 
-        if($this->getParameter('handleLogoutRequest')) {
+        if ($this->getParameter('handleLogoutRequest')) {
             if ($request->request->has('logoutRequest')) {
                 $this->checkHandleLogout($request);
             }
@@ -80,28 +83,28 @@ class CasAuthenticator extends AbstractAuthenticator {
             \phpCAS::handleLogoutRequests(false);
         }
 
-        
+
         // si le mode gateway est activé..
         if ($this->getParameter('gateway')) {
-            
+
             // .. code de pierre pelisset (pour les applis existantes...)
-            
-            if($this->getParameter('force')) {
+
+            if ($this->getParameter('force')) {
                 \phpCAS::forceAuthentication();
                 $user = \phpCAS::getUser();
                 //$force = true;
             } else {
                 //$force = false;
                 //if(!isset($_SESSION['cas_user'])) {
-                    $auth = \phpCAS::checkAuthentication();
-                    if($auth) {
-                        //$_SESSION['cas_user'] = \phpCAS::getUser();
-                        $user = \phpCAS::getUser();
-                        //$_SESSION['cas_attributes'] = \phpCAS::getAttributes();
-                    } else {
-                        //$_SESSION['cas_user'] = false;
-                        $user = "__NO_USER__";
-                    }
+                $auth = \phpCAS::checkAuthentication();
+                if ($auth) {
+                    //$_SESSION['cas_user'] = \phpCAS::getUser();
+                    $user = \phpCAS::getUser();
+                    //$_SESSION['cas_attributes'] = \phpCAS::getAttributes();
+                } else {
+                    //$_SESSION['cas_user'] = false;
+                    $user = "__NO_USER__";
+                }
                 //}
             }
             /*if(!$force) {
@@ -112,37 +115,40 @@ class CasAuthenticator extends AbstractAuthenticator {
                 }
                 
             }*/
-            
-        } else { 
-        
+        } else {
+
             // .. sinon code de david .. pour les api rest / microservices et donc le nouvel ent ulille en view js notamment
-            
-            if($this->getParameter('force')) {
+
+            if ($this->getParameter('force')) {
                 \phpCAS::forceAuthentication();
                 $user = \phpCAS::getUser();
             } else {
-                $authenticated = false;                      
-                if($this->getParameter('gateway')) {
+                $authenticated = false;
+                if ($this->getParameter('gateway')) {
                     $authenticated = \phpCAS::checkAuthentication();
                 } else {
                     $authenticated = \phpCAS::isAuthenticated();
                 }
-                
+
                 //if ( (!isset($_SESSION['cas_user'])) || ( (isset($_SESSION['cas_user'])) && ($_SESSION['cas_user'] != false) ) ) {
-                    
-                    if($authenticated) {
-                        //$_SESSION['cas_user'] = \phpCAS::getUser();
-                        //$_SESSION['cas_attributes'] = \phpCAS::getAttributes();
-                        //$user = $_SESSION['cas_user'];
-                        $user = \phpCAS::getUser();
-                    } else {
-                        $user = "__NO_USER__";
-                    }
+
+                if ($authenticated) {
+                    //$_SESSION['cas_user'] = \phpCAS::getUser();
+                    //$_SESSION['cas_attributes'] = \phpCAS::getAttributes();
+                    //$user = $_SESSION['cas_user'];
+                    $user = \phpCAS::getUser();
+                } else {
+                    $user = "__NO_USER__";
+                }
                 //}
-            } 
+            }
         }
 
-        $passport = new SelfValidatingPassport(new UserBadge($user), []);        
+        // save attributes to get them in UserProvider
+        $attributes = \phpCAS::getAttributes();
+        $this->casAttributeStorage->setAttributes($attributes);
+
+        $passport = new SelfValidatingPassport(new UserBadge($user), []);
 
         return $passport;
     }
@@ -163,7 +169,7 @@ class CasAuthenticator extends AbstractAuthenticator {
                 $user->setCasAttributes(\phpCAS::getAttributes());
             }
         }
-        
+
         return null;
     }
 
@@ -178,25 +184,27 @@ class CasAuthenticator extends AbstractAuthenticator {
         //echo "onAuthenticationFailure<br />";
         $message = strtr($exception->getMessageKey(), $exception->getMessageData());
         $def_response = new Response($message, 403);
-        
-        $event = new CasAuthenticationFailureEvent($request,$exception, $def_response);
+
+        $event = new CasAuthenticationFailureEvent($request, $exception, $def_response);
         $this->eventDispatcher->dispatch($event, CasAuthenticationFailureEvent::POST_MESSAGE);
 
         return $event->getResponse();
     }
 
-    public function getParameter($key) {
-        if(!array_key_exists($key, $this->config)) {
+    public function getParameter($key)
+    {
+        if (!array_key_exists($key, $this->config)) {
             throw new InvalidConfigurationException('l3_cas_guard.' . $key . ' is not defined');
         }
         return $this->config[$key];
     }
-    
+
     /**
      * Cette fonction sert à vérifier le global logout, PHPCAS n'arrive en effet pas à le gérer étrangement dans Symfony2
      * @param Request $request
      */
-    public function checkHandleLogout(Request $request) {
+    public function checkHandleLogout(Request $request)
+    {
         // Récupération du paramètre
         $logoutRequest = $request->request->get('logoutRequest');
         // Les chaines recherchés
@@ -206,7 +214,7 @@ class CasAuthenticator extends AbstractAuthenticator {
         // Isolation de la clé de session
         $begin = strpos($logoutRequest, $open);
         $end = strpos($logoutRequest, $close, $begin);
-        $sessionID = substr($logoutRequest, $begin+strlen($open), $end-strlen($close)-$begin+1);
+        $sessionID = substr($logoutRequest, $begin + strlen($open), $end - strlen($close) - $begin + 1);
 
         // Changement de session et destruction pour forcer l'authentification CAS à la prochaine visite
         session_start();
